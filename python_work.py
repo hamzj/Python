3cc10a10f4a6675015bfb822bd23c9735a028f40
zhangjie <1819237625@qq.com>
2017-12-03 22:17:36 +0800

    first commit

---
+++ python_work.py
@@ -0,0 +1,30 @@
+# 使用“方法”修改字符串的大小写
+first_name = "ada"
+last_name = "love"
+
+
+# 字符串的拼接
+full_name = first_name+" " +last_name
+print("hello,"+full_name.title()+"!")
+
+
+#制表符和换行符的使用
+print("\n\tzhang jie") 
+
+
+#删除空白    
+'''开头空白（lstrip）  
+      尾空白（rstrip）
+      两端空白（strip）'''
+favorite_language = "     python     "
+print(favorite_language)
+favorite_language = favorite_language.strip()
+print(favorite_language)
+
+
+# 单引号和撇号一起用要慎重
+# message = 'One of python's strengths is its diverse community.'
+# 上面代码三个单引号，在解释器看来语法错误
+
+
+
